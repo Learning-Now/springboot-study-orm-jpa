f32d115f1231bedb73342b2d4435900f7621e326
TaeJeong Kim <xowjd41@naver.com>
2021-07-25 21:05:28 +0900

    feat: 3주차 base code

---
+++ week2/src/main/java/com/livenow/week2/domain/Team.java
@@ -0,0 +1,38 @@
+package com.livenow.week2.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.PERSIST)
+    private final List<Member> members = new ArrayList<>();
+
+    @Builder
+    public Team(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void addMember(Member member) {
+        member.changeTeam(this);
+        if (!this.members.contains(member)) {
+            this.members.add(member);
+        }
+    }
+}
