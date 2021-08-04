ecd3ac17e52eb34ac69584a9989550a4c8e7bc1e
DongLee99 <jhnj741@naver.com>
2021-08-04 21:28:24 +0900

    feat : jpashop 초기

---
+++ week5/jpashop/src/test/java/com/livenow/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenow.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
