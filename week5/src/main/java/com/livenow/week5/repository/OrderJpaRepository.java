ee7ed18464cffdb7bc128d5c267d6197d75750c4
DongLee99 <jhnj741@naver.com>
2021-08-19 20:11:22 +0900

    feat : OrderSpec 구현

--- week5/src/main/java/com/livenow/week5/repository/OrderJpaRepository.java
+++ week5/src/main/java/com/livenow/week5/repository/OrderJpaRepository.java
@@ -2,11 +2,11 @@ package com.livenow.week5.repository;
 import com.livenow.week5.domain.Member;
 import com.livenow.week5.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+public interface OrderJpaRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 
     List<Order> findByMember(Member member);
 
