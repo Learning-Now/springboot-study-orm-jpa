bddaa5206dd25eeb7c0438ba3c1cebf65f6f9629
DongLee99 <jhnj741@naver.com>
2021-08-24 11:12:52 +0900

    feat : Calculator Number객체 구현

---
+++ week7/livenow/src/main/java/calculator/domain/Number.java
@@ -0,0 +1,16 @@
+package calculator.domain;
+
+public class Number {
+    private final int value;
+
+    public Number(int input) {
+        validateNumber(input);
+        this.value = input;
+    }
+
+    private void validateNumber(int input) {
+        if (input < 0) {
+            throw new RuntimeException("Error : 음수 입력");
+        }
+    }
+}
