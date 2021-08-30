1bf297ced9e0bd66f4299ec885c0d25730dd4fbd
DongLee99 <jhnj741@naver.com>
2021-08-30 20:53:04 +0900

    test : 1,2를 입력 받았을때 테스트

--- practice/tdd/src/main/java/service/StringCalculator.java
+++ practice/tdd/src/main/java/service/StringCalculator.java
@@ -1,11 +1,23 @@
 package service;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public StringCalculator() {
     }
 
     public int add(String input) {
-        return 0;
+        if (input.contains(",")) {
+            String[] numbers = input.split(",");
+            return sum(numbers);
+        }
+        return Integer.parseInt(input);
+    }
+
+    private int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(number -> Integer.parseInt(number))
+                .sum();
     }
 }
--- practice/tdd/src/test/java/service/StringCalculatorTest.java
+++ practice/tdd/src/test/java/service/StringCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +27,16 @@ class StringCalculatorTest {
         int result = stringCalculator.add(input);
         //then
         assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("1,2 입력받았을경우 테스트")
+    @Test
+    void inputDualNumberWithPattern() {
+        //given
+        String input = "1,2";
+        //when
+        int result = stringCalculator.add(input);
+        //then
+        assertThat(result).isEqualTo(3);
     }
 }
