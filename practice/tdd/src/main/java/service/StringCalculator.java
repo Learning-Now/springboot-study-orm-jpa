d1b03861ba70042204e4ff7a5ba6fa8c6bd51e3b
DongLee99 <jhnj741@naver.com>
2021-08-30 21:25:24 +0900

    refactor : 인스턴스변수를 상수화

--- practice/tdd/src/main/java/service/StringCalculator.java
+++ practice/tdd/src/main/java/service/StringCalculator.java
@@ -3,19 +3,20 @@ package service;
 import java.util.Arrays;
 
 public class StringCalculator {
+    private static final String DEFAULT_PATTERN = "[,;]";
 
     public StringCalculator() {
     }
 
     public int add(String input) {
-        if (input == ""){
+        if (input.isEmpty()){
             return 0;
         }
         return sum(split(input));
     }
 
     private String[] split(String input) {
-        String[] numbers = input.split("[,;]");
+        String[] numbers = input.split(DEFAULT_PATTERN);
         return numbers;
     }
 
