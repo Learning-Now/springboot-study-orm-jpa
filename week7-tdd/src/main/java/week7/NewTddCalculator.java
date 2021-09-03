45c7359ab16fd391c0fe209c26884d8984bd1a20
jeongyuneo <piq2255@gmail.com>
2021-09-03 12:48:21 +0900

    feat: 커스텀 구분자 지정

--- week7-tdd/src/main/java/week7/NewTddCalculator.java
+++ week7-tdd/src/main/java/week7/NewTddCalculator.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NewTddCalculator {
+
     public int add(String data) {
         if (data.isEmpty()) {
             return 0;
         }
-        List<String> numbers = Arrays.asList(data.split(",|:"));
+        String delimiter = ",|:";
+        if (data.startsWith("//")) {
+            delimiter += "|" + data.substring(data.indexOf("//")+2, data.indexOf("\n"));
+            data = data.substring(data.indexOf("\n")+1);
+        }
+        List<String> numbers = Arrays.asList(data.split(delimiter));
         int result = 0;
         for (String number : numbers) {
             result += Integer.parseInt(number);
