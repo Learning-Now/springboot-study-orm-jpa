70455207ca6b20e7943e43db946e28495e8b6639
DongLee99 <jhnj741@naver.com>
2021-09-06 12:56:51 +0900

    refactor : 메서드 선언순서 수정

--- practice/tdd/src/main/java/domain/Number.java
+++ practice/tdd/src/main/java/domain/Number.java
@@ -5,17 +5,24 @@ public class Number {
     private final int value;
 
     public Number(String stringNumber) {
-        validateNumber(stringNumber);
-        value = Integer.parseInt(stringNumber);
+        this(stringToInt(stringNumber));
+    }
+
+    public Number(int number) {
+        this.value = number;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private static int stringToInt(String stringNumber) {
+        return Integer.parseInt(stringNumber);
     }
 
     private void validateNumber(String stringNumber) {
         if(!stringNumber.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException("Error : 숫자가 아닌값이 입력되었습니다.");
         };
-    }
-
-    public int getValue() {
-        return value;
     }
 }
