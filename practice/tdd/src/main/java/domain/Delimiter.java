d9902ca373f589d87eb163274c6f2e71d0fce5d0
DongLee99 <jhnj741@naver.com>
2021-09-13 12:36:29 +0900

    refactor : add의 두개의 check를 delimiter로 넘겨주어 생성시 체크하게 만들어줌

--- practice/tdd/src/main/java/domain/Delimiter.java
+++ practice/tdd/src/main/java/domain/Delimiter.java
@@ -1,42 +1,16 @@
 package domain;
 
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Delimiter {
-    private static final String DEFAULT_PATTERN = "[,;]";
-    private static final String CUSTOM_PATTERN = "^//(.)\n";
-    private static final int GET_DELIMITER_COUNT = 1;
-    private String value;
+    private final String value;
 
-    public Delimiter(String input) {
-        if (!checkCustom(input)) {
-            this.value = DEFAULT_PATTERN;
-        }
+    public Delimiter(String value) {
+        this.value = value;
     }
 
     public String[] split(String input) {
-        if (!checkDefaultValue()) {
-            return input.split(CUSTOM_PATTERN)[1].split(this.value);
-        }
         return input.split(this.value);
-    }
-
-    private boolean checkDefaultValue() {
-        if (this.value.equals(DEFAULT_PATTERN)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean checkCustom(String input) {
-        Matcher matcher = Pattern.compile(CUSTOM_PATTERN).matcher(input);
-        if (matcher.find()) {
-            this.value = matcher.group(GET_DELIMITER_COUNT);
-            return true;
-        }
-        return false;
     }
 
     @Override
--- practice/tdd/src/main/java/service/StringCalculator.java
+++ practice/tdd/src/main/java/service/StringCalculator.java
@@ -3,8 +3,16 @@ package service;
 import domain.Delimiter;
 import domain.Numbers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    private static final String DEFAULT_PATTERN = "[,;]";
+    private static final String CUSTOM_PATTERN = "^//(.)\n";
     private static final String BLANK = "";
+    private static final int GET_DELIMITER_COUNT = 1;
+    private Delimiter customDelimiter;
+    private Delimiter defaultDelimiter = new Delimiter(DEFAULT_PATTERN);
 
     public StringCalculator() {
     }
@@ -13,10 +21,23 @@ public class StringCalculator {
         if (isEqualsBlank(input)){
             return 0;
         }
-        return new Numbers(new Delimiter(input).split(input)).sum();
+        if (checkCustom(input)) {
+            return new Numbers(customDelimiter.split(input)).sum();
+        }
+        return new Numbers(defaultDelimiter.split(input)).sum();
     }
 
     private boolean isEqualsBlank(String input) {
         return BLANK.equals(input);
+    }
+
+    private boolean checkCustom(String input) {
+        Pattern pattern = Pattern.compile(CUSTOM_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            customDelimiter = new Delimiter(matcher.group(GET_DELIMITER_COUNT));
+            return true;
+        }
+        return false;
     }
 }
