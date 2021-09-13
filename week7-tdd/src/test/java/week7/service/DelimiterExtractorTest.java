d87f41d19ec4e901b13a9bbc1aa83c39d455d99e
jeongyuneo <piq2255@gmail.com>
2021-09-13 16:43:11 +0900

    test: DelimiterExtractorTest 메소드 매개변수 수정

--- week7-tdd/src/test/java/week7/service/DelimiterExtractorTest.java
+++ week7-tdd/src/test/java/week7/service/DelimiterExtractorTest.java
@@ -8,16 +8,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DelimiterExtractorTest {
 
-    private static final String FIRST_SEPARATOR = "//";
-    private static final String LAST_SEPARATOR = "\n";
-
     @DisplayName("구분자 추출 테스트")
     @Test
     public void makeDelimiterTest() {
         // given
-        String input = "//;\n1,2,3";
+        String input = ";";
         // when
-        String delimiter = DelimiterExtractor.makeDelimiter(FIRST_SEPARATOR, LAST_SEPARATOR, input);
+        String delimiter = DelimiterExtractor.makeDelimiter(input);
         // then
         assertThat(delimiter).isEqualTo(",|:|;");
     }
