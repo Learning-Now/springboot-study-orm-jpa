487dff0867094b3ebffda548e79025be9b1b0169
woogie <59531827+malpipapi@users.noreply.github.com>
2021-09-25 05:01:18 +0900

    Delete week5 directory

--- week5/src/main/java/com/livenow/week5/domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package com.livenow.week5.domain;
-
-public enum DeliveryStatus {    //comp는 배송완료
-    READY, COMP
-}
--- week5/src/main/java/com/livenow/week5/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.livenow.week5.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
